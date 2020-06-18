ff3d28e3fd2235a0ebce52d81d2853fd2bb3b746
Aaron Sutula <hi@asutula.com>
2020-06-18 13:53:20 -0500

    log some info about cli params (#26)
    * log some info about params
    
    Signed-off-by: Aaron Sutula <hi@asutula.com>
    
    * better logger name
    
    Signed-off-by: Aaron Sutula <hi@asutula.com>

--- main.go
+++ main.go
@@ -13,11 +13,17 @@ import (
 )
 
 var (
-	log    = logging.Logger("main")
+	log    = logging.Logger("devnet-main")
 	config = viper.New()
 )
 
 func main() {
+
+	err := logging.SetLogLevel("devnet-main", "INFO")
+	if err != nil {
+		panic(err)
+	}
+
 	pflag.Int("numminers", 1, "Number of miners in devnet")
 	pflag.Int("speed", ldevnet.DefaultDurationMs, "Chain speed block creation in ms")
 	pflag.Bool("bigsectors", true, "Use big sectors")
@@ -33,7 +39,9 @@ func main() {
 	bigSectors := config.GetBool("bigsectors")
 	ipfsAddr := config.GetString("ipfsaddr")
 
-	_, err := ldevnet.New(numMiners, time.Millisecond*time.Duration(speed), bigSectors, ipfsAddr)
+	log.Infof("Starting devnet with speed = %v, numminers = %v, bigsectors = %v, ipfsaddr = %v", speed, numMiners, bigSectors, ipfsAddr)
+
+	_, err = ldevnet.New(numMiners, time.Millisecond*time.Duration(speed), bigSectors, ipfsAddr)
 	if err != nil {
 		panic(err)
 	}
